ecd1176578dd8b7982e9c9a7457751d4328f27d9
Marian Zharchynskyi <marianzharchynskyi@gmail.com>
2025-09-13 10:13:53 +0300

    feat: added housing-location interface and instance

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HousingLocation } from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
   imports: [HousingLocation],
   template: `
-    <section>
+     <section>
       <form>
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
@@ -15,43 +16,21 @@ import { HousingLocation } from '../housing-location/housing-location';
       <app-housing-location></app-housing-location>
     </section>
   `,
-  styles: `.results {
-  display: grid;
-  column-gap: 14px;
-  row-gap: 14px;
-  grid-template-columns: repeat(auto-fill, minmax(400px, 400px));
-  margin-top: 50px;
-  justify-content: space-around;
-}
-input[type="text"] {
-  border: solid 1px var(--primary-color);
-  padding: 10px;
-  border-radius: 8px;
-  margin-right: 4px;
-  display: inline-block;
-  width: 30%;
-}
-button {
-  padding: 10px;
-  border: solid 1px var(--primary-color);
-  background: var(--primary-color);
-  color: white;
-  border-radius: 8px;
-}
-@media (min-width: 500px) and (max-width: 768px) {
-  .results {
-      grid-template-columns: repeat(2, 1fr);
-  }
-  input[type="text"] {
-      width: 70%;
-  }   
-}
-@media (max-width: 499px) {
-  .results {
-      grid-template-columns: 1fr;
-  }    
-}`
+  styleUrls: ['./home.css'],
 })
 export class Home {
+
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
